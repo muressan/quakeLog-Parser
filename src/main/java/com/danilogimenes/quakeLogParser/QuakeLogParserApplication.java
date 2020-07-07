4677072dbd85c04bdfb31753420a5525bacb0c83
Danilo Gimenes <danilogimenes@orgsystem.com.br>
2020-07-07 02:11:38 -0300

    Configurado Maven Project com Swagger.

---
+++ src/main/java/com/danilogimenes/quakeLogParser/QuakeLogParserApplication.java
@@ -0,0 +1,13 @@
+package com.danilogimenes.quakeLogParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuakeLogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuakeLogParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilogimenes/quakeLogParser/QuakeLogParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilogimenes.quakeLogParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuakeLogParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
